25b85fe68da4375ac10d46a102e462e20bad6e0c
maxiliarias <maxiliarias@gmail.com>
2017-06-30 15:31:55 -0700

    part2react

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives Maxi!</p>,
+const dummyData= [
+  'Do Dishes',
+  'Call Sisco',
+  'Make Reservations',
+  'Do laundry',
+  'Buy Groceries',
+  'Bake Pie'
+]
+
+class Todo extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <li><button>X</button>{this.props.task}</li>
+    )
+  }
+}
+
+class TodoList extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <ul>
+        {this.props.todo.map((x) => <Todo task={x} />)}
+      </ul>
+
+    )
+  }
+}
+
+ReactDOM.render(<TodoList todo={dummyData}/>,
    document.getElementById('root'));
