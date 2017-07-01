82b1856d104a3ce969d79a685561f83ef86c2082
maxiliarias <maxiliarias@gmail.com>
2017-06-30 20:13:16 -0700

    part3

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -4,17 +4,36 @@ import ReactDOM from 'react-dom';
 class InputLine extends React.Component{
   constructor(props){
     super(props);
+    this.state={
+      typedText:""
+    }
   }
+
+  handleTyping(e){
+    this.setState({
+      typedText: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    this.props.submit(this.state.typedText)
+    this.setState({
+      typedText: ""
+    })
+  }
+
   render(){
     return(
       <form>
-      <input type="text" placeholder="task"/>
+      <input type="text" placeholder="task"
+        onChange={(e) => this.handleTyping(e)}
+        value={this.state.typedText}/>
       <input
         type="submit"
-        value="Add todo"
-        onClick={(event) => {
-          event.preventDefault()
-          this.props.submit("test task")
+        value="Add a Task"
+        onClick={(e) => {
+          e.preventDefault()
+          this.handleSubmit(e)
         }}/>
       </form>
       )
